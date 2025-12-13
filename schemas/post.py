242c3884b3f91b48b7812b6c4e444e46caaedd8c
Samuel-pydev <samuel.codes@hotmail.com>
2025-12-13 10:53:51 +0100

    fixed User id

--- schemas/post.py
+++ schemas/post.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional 
 
 class FeedPostCreate(BaseModel):
     text: str
 
 class FeedPostResponse(BaseModel):
     id: int
-    user_id: int  # ADDED THIS LINE
+    user_id: Optional[int] = None
     text: str
     timestamp: datetime
     
